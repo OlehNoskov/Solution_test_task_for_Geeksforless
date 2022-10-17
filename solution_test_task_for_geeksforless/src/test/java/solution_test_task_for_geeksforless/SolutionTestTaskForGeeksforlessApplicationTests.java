a07b0f4686256e9141060253ee1629a207ec1c40
OlehNoskov <olega6000@gmail.com>
2022-10-17 12:49:49 +0300

    update project

---
+++ solution_test_task_for_geeksforless/src/test/java/solution_test_task_for_geeksforless/SolutionTestTaskForGeeksforlessApplicationTests.java
@@ -0,0 +1,13 @@
+package solution_test_task_for_geeksforless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionTestTaskForGeeksforlessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
